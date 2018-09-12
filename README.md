f7f4bba4f4e1b17b11609459ef8231c9365555d7
Tom Saleeba <tom.saleeba@gmail.com>
2018-09-12 17:00:16 +0930

    feat: first working version
    added support for deleting records too

--- README.md
+++ README.md
@@ -12,9 +12,24 @@
       # replace with your values
       export DHIS_USER=someuser
       export DHIS_PASS=somepass
+      export DHIS_RECORD_COUNT=100 # records to create
       ```
-  1. run the script to create and enroll a TEI (Tracked Entity Instance)
+  1. run the script to create and enroll the number of TEIs (Tracked Entity Instances) specified by the count (default is 1)
       ```
       node index.js
       ```
 
+## Deleting records
+This script can also delete records all the records has created (based on the prefix to Full Name). You can run in `delete` mode with:
+
+  1. clone and install deps as above
+  1. define some environment variables to override the defaults
+      ```bash
+      # replace with your values
+      export DHIS_USER=someuser
+      export DHIS_PASS=somepass
+      ```
+  1. run the script in delete mode
+      ```bash
+      node index.js delete
+      ```
--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const axios = require('axios')
 const VError = require('verror')
+const chalk = require('chalk')
+const dateGenerator = require('random-date-generator')
+const yyyymmdd = require('yyyy-mm-dd')
 
 const urlBase = process.env.DHIS_URLBASE || 'http://dhis.nacopha.techotom.com'
 const apiUrl = `${urlBase}/api/29`
 const user = process.env.DHIS_USER || 'admin'
-const pass = process.env.DHIS_PASS || 'password'
+const pass = process.env.DHIS_PASS || 'district'
+const recordsToCreate = process.env.DHIS_RECORD_COUNT || 1
 const fullNamePrefix = 'test-'
+const pageSize = 200
+
+let runMode = 'create'
+if ('delete'.startsWith(process.argv[2])) {
+  runMode = 'delete'
+}
 
 console.log(`Using config:
   url : ${apiUrl}
   user: ${user}
-  pass: ${pass}`)
+  pass: ${pass}
+  mode: ${runMode}
+  recs: ${recordsToCreate} records will be created (if in create mode)
+`)
 
 const authConfig = {
     username: user,
     password: pass,
   }
 const getConfig = {
+  auth: authConfig
+}
+const deleteConfig = {
   auth: authConfig
 }
 const postConfig = {
@@ -60,89 +76,284 @@ const empowermentGroupNames = {
 // trackedEntityTypes, api/29/trackedEntityTypes.json
 const person = 'kJQnjvFXP18'
 
-const uniqueFragment = new Date().getTime()
+const strategies = {
+  create: createStrategy,
+  delete: deleteStrategy,
+}
 
-// getNacophaId
-axios.get(apiUrl + `/trackedEntityAttributes/${nacophaId}/generate`, getConfig)
-  .catch(err => {
-    console.error(new VError(err, 'Failed to generate a NACOPHA ID'))
-    return false
-  })
-  .then(resp => {
-    if (!resp) {
-      return false
+const strategy = strategies[runMode]
+if (!strategy) {
+  console.error(`Unsupported runMode = '${runMode}', exiting.`)
+  process.exit(1)
+}
+strategy().catch(err => {
+  console.error(chalk.red('[ERROR] '), new VError(err))
+})
+
+
+async function createStrategy () {
+  for (let i = 0; i < recordsToCreate; i++) {
+    try {
+      info(`Creating record ${i+1}/${recordsToCreate}`)
+      await createSingleRecord()
+    } catch (err) {
+      throw new VError(err, 'Failed to execute creating a single record')
     }
-    const nextNacophaIdValue = resp.data.value
-    const fullNameValue = fullNamePrefix + 'Test One'
-    // create trackedEntityInstance
-    const createTeiData = {
-      trackedEntityType: person,
-      orgUnit: orgUnits['Arusha CC Konga'],
-      attributes: [
-        {
-          attribute: nacophaId,
-          value: nextNacophaIdValue
-        }, {
-          attribute: ctc,
-          value: `${uniqueFragment}`
-        }, {
-          attribute: fullName,
-          value: fullNameValue
-        }, {
-          attribute: gender,
-          value: genders.male
-        }, {
-          attribute: age,
-          value: '2018-09-03'
-        }, {
-          attribute: initials,
-          value: 'tt'
-        }, {
-          attribute: empowermentGroupRole,
-          value: empowermentGroupRoles.group1
-        }, {
-          attribute: empowermentGroupName,
-          value: empowermentGroupNames.member
-        }
-      ]
-    }
-    console.log(`Creating a TEI with name '${fullNameValue}'`)
-    return axios.post(apiUrl + '/trackedEntityInstances', createTeiData, postConfig)
-  })
-  .catch(err => {
+  }
+}
+
+async function createSingleRecord () {
+  const uniqueFragment = new Date().getTime()
+
+  // getNacophaId
+  let nextNacophaIdValue
+  try {
+    const resp = await axios.get(apiUrl + `/trackedEntityAttributes/${nacophaId}/generate`, getConfig)
+    nextNacophaIdValue = resp.data.value
+  } catch (err) {
+    throw new VError(err, 'Failed to generate a NACOPHA ID')
+  }
+  const nameAndInitials = generateNameAndInitials()
+  const fullNameValue = fullNamePrefix + nameAndInitials.name
+  const randomGender = Math.random() > 0.5 ? genders.male : genders.female
+  const randomDateStr = generateRandomDate()
+  // create trackedEntityInstance
+  const createTeiData = {
+    trackedEntityType: person,
+    orgUnit: orgUnits['Arusha CC Konga'],
+    attributes: [
+      {
+        attribute: nacophaId,
+        value: nextNacophaIdValue
+      }, {
+        attribute: ctc,
+        value: `${uniqueFragment}`
+      }, {
+        attribute: fullName,
+        value: fullNameValue
+      }, {
+        attribute: gender,
+        value: randomGender
+      }, {
+        attribute: age,
+        value: randomDateStr
+      }, {
+        attribute: initials,
+        value: nameAndInitials.initials
+      }, {
+        attribute: empowermentGroupRole,
+        value: empowermentGroupRoles.group1
+      }, {
+        attribute: empowermentGroupName,
+        value: empowermentGroupNames.member
+      }
+    ]
+  }
+  info(`Creating a TEI with name='${fullNameValue}', DOB='${randomDateStr}', gender='${randomGender}', nacophaId='${nextNacophaIdValue}'`)
+  let teiId
+  try {
+    const resp = await axios.post(apiUrl + '/trackedEntityInstances', createTeiData, postConfig)
+    teiId = resp.data.response.importSummaries[0].reference
+  } catch (err) {
     //const errData = err && err.response && err.response.data || false
     //if (errData) {
-      //console.error('====================')
-      //console.error(errData.message)
-      //console.error(errData.response.importSummaries[0].conflicts)
-      //console.error('====================')
+    //console.error('====================')
+    //console.error(errData.message)
+    //console.error(errData.response.importSummaries[0].conflicts)
+    //console.error('====================')
     //}
-    console.error(new VError(err, 'Failed to create trackedEntityInstance'))
-    return false
-  })
-  .then(resp => {
-    if (!resp) {
+    throw new VError(err, 'Failed to create trackedEntityInstance')
+  }
+  // enroll trackedEntityInstance
+  const enrollTeiData = {
+    enrollmentDate: '2018-09-09',
+    incidentDate: '2018-09-10',
+    orgUnit: orgUnits['Arusha CC Konga'],
+    program: programs['Sauti Yetu - [Data Collection]'],
+    status: 'ACTIVE',
+    trackedEntityInstance: teiId
+  }
+  info(`Enrolling ${fullNameValue}`)
+  try {
+    await axios.post(apiUrl + '/enrollments', enrollTeiData, postConfig)
+  } catch (err) {
+    throw new VError(err, `Failed to enroll TEI with ID='${teiId}'`)
+  }
+  info(`Created and enrolled ${fullNameValue}`)
+  return true
+}
+
+async function deleteStrategy () {
+  info('Gathering TEI IDs to delete')
+  const url = apiUrl + `/trackedEntityInstances?ouMode=ALL&filter=${fullName}:LIKE:${fullNamePrefix}&pageSize=${pageSize}`
+  axios.get(url, getConfig)
+    .then(resp => {
+      const teisToDelete = resp.data.trackedEntityInstances.reduce((accum, curr) => {
+        accum.push(curr.trackedEntityInstance)
+        return accum
+      }, [])
+      const foundCount = teisToDelete.length
+      if (foundCount === 0) {
+        info('No TEIs found for deletion, exiting.')
+        return false
+      }
+      info(`Found ${foundCount} TEIs to delete`)
+      if (foundCount >= pageSize) {
+        info('Found a full page of TEIs to delete, you should run this again in case there are more records than our page size')
+      }
+      return teisToDelete
+    })
+    .catch(err => {
+      console.error(new VError(err, 'Failed to get list of TEIs to delete'))
       return false
-    }
-    // enroll trackedEntityInstance
-    const enrollTeiData = {
-      enrollmentDate: '2018-09-09',
-      incidentDate: '2018-09-10',
-      orgUnit: orgUnits['Arusha CC Konga'],
-      program: programs['Sauti Yetu - [Data Collection]'],
-      status: 'ACTIVE',
-      trackedEntityInstance: resp.data.response.importSummaries[0].reference
-    }
-    console.log(`Enrolling TEI`)
-    return axios.post(apiUrl + '/enrollments', enrollTeiData, postConfig)
-  })
-  .catch(err => {
-    console.error(new VError(err, 'Failed to enroll a TEI'))
-    return false
-  })
-  .then(resp => {
-    if (!resp) {
+    })
+    .then(teisToDelete => {
+      if (!teisToDelete) {
+        return false
+      }
+      const deletePromises = teisToDelete.map(curr => axios.delete(apiUrl + `/trackedEntityInstances/${curr}`, deleteConfig))
+      info('Starting delete operations')
+      return Promise.all(deletePromises)
+    })
+    .then(results => {
+      if (!results) {
+        return false
+      }
+      info(`Successfully deleted ${results.length} TEIs`)
+    })
+    .catch(err => {
+      console.error(new VError(err, 'Failed to delete all TEIs, some may have succeeded though'))
       return false
-    }
-    console.log(`Created and enrolled a TEI`)
-  })
+    })
+}
+
+function info (msg) {
+  console.log(chalk.yellow('[INFO] ') + msg)
+}
+
+// thanks http://listofrandomnames.com/index.cfm?textarea
+const firstNames = [
+  'Jamison',
+  'Sharice',
+  'Hermelinda',
+  'Lorenzo',
+  'Mohamed',
+  'Anibal',
+  'Collene',
+  'Artie',
+  'Allena',
+  'Byron',
+  'Jerrell',
+  'Jewell',
+  'Caren',
+  'Nicholle',
+  'Clemencia',
+  'Ariana',
+  'Deandra',
+  'Tegan',
+  'Edie',
+  'Misti',
+  'Clint',
+  'Jacob',
+  'Shanti',
+  'Kami',
+  'Clarita',
+  'Esther',
+  'Hsiu',
+  'Cristopher',
+  'Kaylee',
+  'Loma',
+  'Tabatha',
+  'Taina',
+  'Horace',
+  'Edith',
+  'Lory',
+  'Marlys',
+  'Deborah',
+  'Evangelina',
+  'Lashanda',
+  'Marleen',
+  'Ward',
+  'Leeann',
+  'Shira',
+  'Ivette',
+  'Caryl',
+  'Barbra',
+  'Rosaria',
+  'Daniela',
+  'Sharla',
+  'Mervin',
+]
+
+const surnames = [
+  'Huseby',
+  'Alcott',
+  'Heishman',
+  'Westover',
+  'Montagna',
+  'Elmer',
+  'Gerhard',
+  'Enderle',
+  'Saldana',
+  'Mealy',
+  'Mund',
+  'Followell',
+  'Hollis',
+  'Tootle',
+  'Cowherd',
+  'Register',
+  'Lenard',
+  'Seawood',
+  'Straub',
+  'Viger',
+  'Kan',
+  'Clermont',
+  'Moree',
+  'Stansell',
+  'Moneypenny',
+  'Caplinger',
+  'Poovey',
+  'Rushford',
+  'Oyer',
+  'Kovac',
+  'Roll',
+  'Pitzer',
+  'Creswell',
+  'Huss',
+  'Gullatt',
+  'Stracener',
+  'Noel',
+  'Hoye',
+  'Remington',
+  'Burris',
+  'Wingham',
+  'Steele',
+  'Foushee',
+  'Veliz',
+  'Flickinger',
+  'Jiminez',
+  'Rodman',
+  'Aranda',
+  'Brodie',
+  'Recinos',
+]
+
+function generateNameAndInitials () {
+  const firstNameIndex = Math.floor(Math.random() * firstNames.length)
+  const surnameIndex = Math.floor(Math.random() * surnames.length)
+  const firstNameValue = firstNames[firstNameIndex]
+  const surnameValue = surnames[surnameIndex]
+  return {
+    name: `${firstNameValue} ${surnameValue}`,
+    initials: `${firstNameValue.substr(0,1)}${surnameValue.substr(0,1)}`
+  }
+}
+
+function generateRandomDate () {
+  const startDate = new Date(1950, 1, 1)
+  const endDate = new Date(2017, 5, 5)
+  const r = dateGenerator.getRandomDateInRange(startDate, endDate)
+  //return `${r.getFullYear()}-${r.getMonth()}-${r.getDay()}`
+  return yyyymmdd(r)
+}
+
